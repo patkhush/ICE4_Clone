3427f8281c0bd21f96879a7e0271053ac8b03ef6
Khush Patel <pakhushn@sheridancollege.ca>
2026-02-05 19:01:45 -0500

    added Added a multiplication method to the features

---
+++ Project_KhushPatel_Clone/src/main/java/inclass/three/project_khushpatel_clone/Project_KhushPatel_Clone.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package inclass.three.project_khushpatel_clone;
+
+/**
+ *
+ * @author patel
+ */
+public class Project_KhushPatel_Clone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    
+    
+    public double multi(double x, double y){
+        return x*y;
+    }
+    
+}
